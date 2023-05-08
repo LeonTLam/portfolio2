930702fe78b294af6a15e3375960a89a25ee5a18
Leon Tongfai Lam <s352005@oslomet.no>
2023-05-08 16:17:10 +0200

    08.05.2023
    +dtrp code

--- dtrp.py
+++ dtrp.py
@@ -4,6 +4,7 @@ import sys
 import signal
 from struct import *
 
+
 class PortInRangeAction(argparse.Action):
     def __call__(self, parser, namespace, port, option_string=None):
         if port < 1024 or port > 65535:
@@ -38,6 +39,8 @@ def handle_method(server_socket, client_socket, client_address, args):
             msg = packet_create(0, 1, flags, 0, b'')
             client_socket.send(msg)
             print(f'{client_address} <-[ACK]')
+            
+            server_handle_client(server_socket,client_address,client_address,args)
         else:
             print(f"Client's method {msg} is different from server's method {method}\nRestarting...")
             client_socket.close()
@@ -55,6 +58,7 @@ def handle_method(server_socket, client_socket, client_address, args):
         seq, ack, flags, win = header_parse(msg)
         if ack == 1:
             print(f'{client_address} +[ACK]\nBoth methods are valid, continuing...')
+            client_send(client_socket, args)
         else:
             print(f"Your method {method} is different from server's method, make sure you are running the same method as server.\nClosing...")
             client_socket.close()
@@ -64,6 +68,7 @@ def handle_method(server_socket, client_socket, client_address, args):
 # Section to implement and develope the use of headers etc.
 
 header_format = '!IIHH'
+packet_size = 1472
 #creates a packet with header information and application data
     #the input arguments are sequence number, acknowledgment number
     #flags (we only use 4 bits),  receiver window and application data 
@@ -99,7 +104,7 @@ def three_way_handshake(server_socket, client_socket, client_address, args):
         server_socket.settimeout(0.5)
         try:
             # Server receives SYN handshake
-            msg = server_socket.recv(1472).decode()
+            msg = server_socket.recv(packet_size).decode()
             seq, ack, flags, win = header_parse(msg)
             flags = flags_parse(flags)
             
@@ -113,12 +118,11 @@ def three_way_handshake(server_socket, client_socket, client_address, args):
                 print(f'{client_address} <-[SYN-ACK]')
                 
                 # Server receives ACK handshake
-                msg = server_socket.recv(1472).decode()
+                msg = server_socket.recv(packet_size).decode()
                 seq, ack, flags, win = header_parse(msg)
                 if ack == 1:
                     print(f'{client_address} +[ACK]\nClient has connected.')
                     handle_method(server_socket, client_socket, client_address, args)
-                    server_handle_client(server_socket, client_socket, client_address, args)
 
         except socket.timeout:
             print('Error communicating with client, try again.')
@@ -135,7 +139,7 @@ def three_way_handshake(server_socket, client_socket, client_address, args):
             client_socket.send(msg)
             print(f'{args.bind} <-[SYN]')
             # Client receives SYN-ACK handshake
-            msg = client_socket.recv(1472).decode()
+            msg = client_socket.recv(packet_size).decode()
             seq, ack, flags, win = header_parse(msg)
             flags = flags_parse(flags)
 
@@ -148,8 +152,7 @@ def three_way_handshake(server_socket, client_socket, client_address, args):
                 client_socket.send(msg)
                 print(f'{args.bind} <-[ACK]')
                 
-                handle_method(server_socket, client_socket, client_address, args)
-                client_send(None, client_socket, None, args)
+                handle_method(None, client_socket, None, args)
 
         except socket.timeout:
             print('Error communication with server, try again')
@@ -161,22 +164,40 @@ def two_way_byeshake(server_socket, client_socket, client_address, args):
     if args.server:
         server_socket.settimeout(0.5)
         try:
-            msg = server_socket.recv(1472).decode()
-
+            msg = server_socket.recv(packet_size).decode()
             seq, ack, flags, win = header_parse(msg)
             flags = flags_parse(flags)
 
             if flags[2] == 2:
-                print('+[FIN]')
+                print(f'{client_address} +[FIN]')
 
                 flags = 0
                 msg = packet_create(0, 1, flags, 0, 0)
                 client_socket.send(msg)
+                print(f'{client_address} <-[ACK]')
+                
+                print(f'Client {client_address} has disconnected')
+                client_socket.close()
         except socket.timeout:
-            print('')
-
+            two_way_byeshake(server_socket,client_socket,client_address,args)
+        
     elif args.client:
         client_socket.settimeout(0.5)
+        try:
+            flags = 2
+            msg = packet_create(0, 0, flags, 0, b'')
+            client_socket.send(msg)
+            print(f'{args.bind} <-[FIN]')
+            
+            msg = client_socket.recv(packet_size).decode()
+            seq, ack, flags, win = header_parse(msg)
+            
+            if ack == 1:
+                print(f'{args.bind} +[ACK]\nClosing...')
+                client_socket.close()
+                sys.exit(1)
+        except socket.timeout:
+            two_way_byeshake(None,client_socket,None,args)
 # Section for starting server and client
 
 def server_start(args: argparse.Namespace):
@@ -189,15 +210,14 @@ def server_start(args: argparse.Namespace):
         server_socket.bind((server_host,server_port))
         server_socket.listen(1)
         
-        print('---------------------------------------')
-        print(f'Server is listening on port {server_port}')
-        print('---------------------------------------')
-    
         try:
             while True:
+                print('---------------------------------------')
+                print(f'Server is listening on port {server_port}')
+                print('---------------------------------------')
                 client_socket, client_address = server_socket.accept()
                 three_way_handshake(server_socket, client_socket, client_address, args)
-                two_way_byeshake(server_socket, client_socket, client_address, args)
+                
         
         except KeyboardInterrupt:
             print('Closing server')
@@ -205,7 +225,49 @@ def server_start(args: argparse.Namespace):
             sys.exit(1)
             
 def server_handle_client(server_socket, client_socket, client_address, args):
+    server_socket.settimeout(0.5)
     
+    dataArray = []
+    def send_and_wait(seq_num):
+        while True:
+            try:
+                msg = server_socket.recv(packet_size).decode()
+                seq, ack, flags, win= header_parse(msg)
+                flags = flags_parse(flags)
+                
+                if seq == seq_num:
+                    print(f'{client_address} +[PACKET #{seq}]')
+                    data_msg = msg[12:]
+                    dataArray.append(data_msg)
+                    
+                    msg = packet_create(0, 1, 0, 0, b'')
+                    client_socket.send(msg)
+                    print(f'{client_address} +[ACK #{seq_num}]')
+                    seq_num += 1
+                
+                elif seq != seq_num:
+                    msg = packet_create(seq_num-1, 1, 0, 0, b'')
+                    client_socket.send(msg)
+                    print(f'{client_address} +[DUPACK #{seq_num - 1}]')
+                
+                elif flags[2] == 2:
+                    print(f'{client_address} +[FIN]')
+
+                    msg = packet_create(0, 1, 0, 0, 0)
+                    client_socket.send(msg)
+                    print(f'{client_address} <-[ACK]')
+                    
+                    print(f'Client {client_address} has disconnected')
+                    client_socket.close()
+                    break
+            except socket.timeout:
+                send_and_wait(seq_num)            
+        return dataArray
+    
+    with open(args.file, 'wb') as file:
+        for data in dataArray:
+            file.write(data)
+
 def client_connect(args: argparse.Namespace):
 
     server_host = args.ip
@@ -218,9 +280,30 @@ def client_connect(args: argparse.Namespace):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((server_host,server_port))
         three_way_handshake(None, client_socket, None, args)
-        two_way_byeshake(None, client_socket, None, args)
+        
 def client_send(client_socket, args: argparse.Namespace):
-
+    client_socket.settimeout(0.5)
+    
+    dataArray = []
+    
+    with open(args.file, 'rb') as file:
+        while True:
+            try:
+                data = file.read(1460)
+                dataArray.append(data)
+                if not data:
+                    break
+            except IOError as e:
+                print(f'An IOerror occured: {e}')
+                client_socket.close()
+                sys.exit(1)
+    
+    def send_and_wait(seq_num):
+        for seq_num in range(len(dataArray)):
+            try:
+                msg = packet_create(seq_num, 0, 0, 1, dataArray[seq_num])
+                client_socket.send(msg)
+            except:
 def main():
     
     parser = argparse.ArgumentParser(description="File transferring application over 'DRTP'.")
--- test.py
+++ test.py
@@ -1,23 +1,30 @@
-def parse_flags(flags):
-    #we only parse the first 3 fields because we're not 
-    #using rst in our implementation
-        syn = flags & (1 << 3)
-        ack = flags & (1 << 2)
-        fin = flags & (1 << 1)
-        res = flags & (1 << 0)
-        return syn, ack, fin, res
+#def parse_flags(flags):
+#    #we only parse the first 3 fields because we're not 
+#    #using rst in our implementation
+#        syn = flags & (1 << 3)
+#        ack = flags & (1 << 2)
+#        fin = flags & (1 << 1)
+#        res = flags & (1 << 0)
+#        return syn, ack, fin, res
+#
+#flags = 12
+#
+#flags = parse_flags(flags)
+#print(f"{flags[1]}")
+#
+#def test(noe, noe1, noe2):
+#    if noe:
+#        print(1)
+#    elif noe1:
+#        print(2)
+#    elif noe2:
+#        print(3)
+#
+#test(None, True, None)
 
-flags = 12
-
-flags = parse_flags(flags)
-print(f"{flags[1]}")
-
-def test(noe, noe1, noe2):
-    if noe:
+dataArray = [1, 2, 3]
+def test(seq):
+    while seq <= len(dataArray):
         print(1)
-    elif noe1:
-        print(2)
-    elif noe2:
-        print(3)
-
-test(None, True, None)
+        seq +=1
+test(3)
