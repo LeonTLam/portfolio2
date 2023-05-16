ea28d5e303f09f664e20e1e8e0a1d68f234317e9
Leon Tongfai Lam <s352005@oslomet.no>
2023-05-16 08:26:00 +0200

    Update dtrp.py

--- dtrp.py
+++ dtrp.py
@@ -5,19 +5,25 @@ import time
 import os
 from struct import *
 
-
+# Description:
+# Custom action to check if port is within valid range ∊ [1024, 65535]
+# Arguments:
+# args.port or port: holds the port retrieved from command-line option
+# __call__: Method to be called when corresponding command-line argument is encountered
+# namespace: When action is triggered, value port is set to namespace object (args.port)
+# Returns ArgumentError if out of range; Returns port if valid
 class PortInRangeAction(argparse.Action):
     def __call__(self, parser, namespace, port, option_string=None):
         if port < 1024 or port > 65535:
             raise argparse.ArgumentError(self, f"{port} is not in range of [1024, 65535]")
         setattr(namespace, self.dest, port)
-        
-class TestInRangeAction(argparse.Action):
-    def __call__(self, parser, namespace, test, option_string=None):
-        if test < 1 or test > 3:
-            raise argparse.ArgumentError(self, f"{test} is not in range of [1, 3]")
-        setattr(namespace, self.dest, test)
-        
+
+# Description
+# Custom action to check if args.reliable_method is valid
+# Arguments:
+# method: holds string args.reliable_method without whitespace and in lower capitalization
+# methods: dictionary with valid string to be entered
+# Returns ArgumentError if method is invalid; Returns method in uppercase and shortened if valid
 class ValidMethodAction(argparse.Action):
     def __call__(self, parser, namespace, method, option_string=None):
         method = method.strip().lower()
@@ -26,50 +32,82 @@ class ValidMethodAction(argparse.Action):
             raise argparse.ArgumentError(self, f'{method} is an invalid method')
         setattr(namespace, self.dest, methods[method])
 
+# Description:
+# Function to handle if both server and client are invoked with same reliable method
+# Arguments:
+# server_socket: holds the binded server connection
+# client_socket: holds the connected client
+# client_address: holds the connected client's address and port
+# args: holds the server arguments with the same object-names
 def handle_method(server_socket, client_socket, client_address, args):
+    
+    # If invoked as server
     if args.server:
+        # Store reliable method
         method = args.reliable_method
         print(f'Reliable method: {method}')
         
-        msg = client_socket.recv(1024).decode()
+        # Receive client's reliable method
+        msg = client_socket.recv(packet_size).decode()
         print(f'{client_address} +[method]')
+        
+        # If both methods match, send ACK, then call function server_handle_client()
         if msg == method:
             send_ack(client_socket, client_address, args)
             print('Both methods are valid, continuing...')
-            
-            
-            
             server_handle_client(server_socket,client_socket,client_address,args)
         else:
+        # If methods do not match, exit
             print(f"Client's method {msg} is different from server's method {method}\nRestarting...")
             client_socket.close()
             server_socket.close()
             server_start(args)
     
+    # If invoked as client
     elif args.client:
+        # Store reliable method
         method = args.reliable_method
         print(f'Reliable method: {method}')
         
+        # Send reliable method
         client_socket.send(method.encode())
         print(f'{args.ip} <-[method]')
         
-        msg = client_socket.recv(1024)
+        # Receive msg
+        msg = client_socket.recv(packet_size)
         seq, ack, flags, win = header_parse(msg)
+        # Receive ACK, call function client_send()
         if ack == 1:
             print(f'{args.ip} +[ACK]\nBoth methods are valid, continuing...')
             client_send(client_socket, args)
         else:
+        # If not ACK, exit
             print(f"Your method {method} is different from server's method, make sure you are running the same method as server.\nClosing...")
             client_socket.close()
             sys.exit(1)            
             
 
 
-
+# Description:
+# Structure for header (12 bytes)
+# Arguments:
+# '!' Byte order, network (= big-endian)
+# 'I' Unsigned int, 4 bytes
+# 'H' Unsigned short int, 2 bytes
 header_format = '!IIHH'
+
+# Packet_size = size_of_data + header_format = 1460 + 12
 packet_size = 1472
 
-
+# Description:
+# Convert values into packed binary format
+# Arguments:
+# Seq: sequence numer
+# Ack: acknowledgment number
+# Flags: conditions of packet
+# Win: window size
+# Data: file data
+# Returns a bytes object packed according to header_format
 def packet_create(seq, ack, flags, win, data):
     
     header = pack(header_format, seq, ack, flags, win)
@@ -78,12 +116,20 @@ def packet_create(seq, ack, flags, win, data):
     
     return packet
 
+# Description:
+# Takes header of 12 bytes and unpacks values
+# Arguments:
+# Header: packet containing a header
+# Returns values in a tuple
 def header_parse(header):
     
     msg_header = unpack(header_format, header[:12])
     
     return msg_header
 
+# Description:
+# Applies certain conditions based on binary
+# Returns values for different indexes, depending on condition applied
 def flags_parse(flags):
 
     syn = flags & (1 << 3)
@@ -93,13 +139,15 @@ def flags_parse(flags):
 
     return syn, ack, fin, res
 
+# Description:
+# Function to establish a reliable connection between server and client
+# If successfully established, call function handle_method()
 def three_way_handshake(server_socket, client_socket, client_address, args):
     
     if args.server:
         server_socket.settimeout(0.5)
         try:
             # Server receives SYN handshake
-            print('test')
             msg = client_socket.recv(packet_size)
             seq, ack, flags, win = header_parse(msg)
             flags = flags_parse(flags)
@@ -150,6 +198,9 @@ def three_way_handshake(server_socket, client_socket, client_address, args):
         client_socket.close()
         sys.exit(1)
 
+# Description:
+# Function to establish termination of connection between server and client
+# If successfully established, close corresponding sockets.
 def two_way_byeshake(server_socket, client_socket, client_address, args):
 
     if args.server:
@@ -180,6 +231,8 @@ def two_way_byeshake(server_socket, client_socket, client_address, args):
         except socket.timeout:
             two_way_byeshake(None,client_socket,None,args)
 
+
+# Function to send ACK-header without application data
 def send_ack(client_socket, client_address, args):
     if args.server:
         msg = packet_create(0, 1, 0, 0, b'')
@@ -189,12 +242,17 @@ def send_ack(client_socket, client_address, args):
         msg = packet_create(0, 1, 0, 0, b'')
         client_socket.send(msg)
         print(f'{args.ip} <-[ACK]')
-    
+
+# Description:
+# Function to send DUPACK-header without application data
+# Arguments:
+# seq_num: Usually missing packet
 def send_dupack(client_socket, client_address, seq_num):
     msg = packet_create(seq_num, 1, 0, 0, b'')
     client_socket.send(msg)
     print(f'{client_address} <-[DUPACK #{seq_num}]')
-    
+
+# Function to start server and listen for 1 client
 def server_start(args: argparse.Namespace):
     
     server_host = args.ip
@@ -211,6 +269,7 @@ def server_start(args: argparse.Namespace):
                 print(f'Server is listening on port {server_port}')
                 print('---------------------------------------')
                 client_socket, client_address = server_socket.accept()
+                # Establish connection
                 three_way_handshake(server_socket, client_socket, client_address, args)
                 break
         
@@ -221,17 +280,22 @@ def server_start(args: argparse.Namespace):
             sys.exit(1)
         
     server_socket.close()
-            
+# Function for server to handle incoming data with requested reliable method
+# Returns file sent by client
 def server_handle_client(server_socket, client_socket, client_address, args):
     dataArray = []
     server_socket.setblocking(False)
+    # Read more under project report
     def send_and_wait(seq_num):
+        
+        # Initialize measuring variables
         throughput_start = time.time()
         packets_recv= 0
         start_time = 0
         
         while True:
             try:
+                # Set the time equal to RTT of a packet, multiplied by 4
                 if start_time != 0:
                     server_socket.settimeout(4 * (time.monotonic() - start_time))
                     
@@ -264,7 +328,7 @@ def server_handle_client(server_socket, client_socket, client_address, args):
                 print(f"Receiver throughput (Send and wait): {throughput} packets/s")    
                 two_way_byeshake(server_socket,client_socket,client_address,args)
                 break
-            
+    # Read more under project report        
     def go_back_n(seq_num):
         throughput_start = time.time()
         packets_recv = 0
@@ -299,13 +363,16 @@ def server_handle_client(server_socket, client_socket, client_address, args):
                 print(f'{client_address} +[FIN]')
                 two_way_byeshake(server_socket,client_socket,client_address,args)
                 break
-        
+    # Read more under project report    
     def go_back_n_sr(seq_num):
+        
         throughput_start = time.time()
         packets_recv = 0
+        
         seq_first = 1
         seq_end = 0
-        tempDataArray = {}
+        
+        tempDataArray = {} # Dictionary of seq-num and data, to be sorted
         
         while True:
             msg = client_socket.recv(packet_size)
@@ -341,6 +408,7 @@ def server_handle_client(server_socket, client_socket, client_address, args):
                     tempDataArray.update({seq:data_msg})
                     send_dupack(client_socket, client_address, seq_num)
         
+        # Sorting by key (seq-number)
         dataArray_sorted = dict(sorted(tempDataArray.items(), key=lambda x: int(x[0])))
             
         for value in dataArray_sorted.values():
@@ -357,11 +425,12 @@ def server_handle_client(server_socket, client_socket, client_address, args):
     elif args.reliable_method == 'GBN-SR':
         go_back_n_sr(1)
             
+    # Write file in same directory with requested name
     with open(os.path.join(os.getcwd(), args.file), 'wb') as file:
         for data in dataArray:
             file.write(data)
 
-    
+# Function to connect the client to server    
 def client_connect(args: argparse.Namespace):
 
     server_host = args.ip
@@ -374,11 +443,12 @@ def client_connect(args: argparse.Namespace):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((server_host,server_port))
         three_way_handshake(None, client_socket, None, args)
-        
-def client_send(client_socket, args: argparse.Namespace):
-    
-    dataArray = []
 
+# Function for client to handle data to be sent with requested reliable method    
+def client_send(client_socket, args: argparse.Namespace):
+    dataArray = []
+    
+    # Reads data onto a list in increments of 1460 bytes
     with open(args.file, 'rb') as file:
         while True:
             try:
@@ -391,6 +461,7 @@ def client_send(client_socket, args: argparse.Namespace):
                 client_socket.close()
                 sys.exit(1)
     
+    # Read more under project report
     def send_and_wait(seq_num):
         throughput_start = time.time()
         packets_sent = 0
@@ -415,17 +486,19 @@ def client_send(client_socket, args: argparse.Namespace):
             except socket.timeout:
                 print('Timeout, retransmitting packets from current window')
                 pass    
-            
+        # Measure throughput by recording how many packets were sent between the time program began and finished    
         throughput = packets_sent / (time.time() - throughput_start)
         print(f"Sender throughput (Send and wait): {throughput} packets/s")        
         two_way_byeshake(None,client_socket,None,args)
-               
+    
+    # Read more under project report           
     def go_back_n(seq_num):
+        throughput_start = time.time()
+        packets_sent = 0
+        
         seq_win = args.window
         seq_first = seq_num
         seq_end = seq_first + seq_win
-        throughput_start = time.time()
-        packets_sent = 0
         while seq_first <= len(dataArray):
             while seq_first <= seq_end:
                 if seq_first > len(dataArray):
@@ -463,7 +536,9 @@ def client_send(client_socket, args: argparse.Namespace):
         throughput = packets_sent / (time.time() - throughput_start)
         print(f"Sender throughput (Go back N): {throughput} packets/s")     
         two_way_byeshake(None,client_socket,None,args)
-        
+    
+    
+    # Read more under project report    
     def go_back_n_sr(seq_num):
         throughput_start = time.time()
         packets_sent = 0
@@ -530,24 +605,26 @@ def client_send(client_socket, args: argparse.Namespace):
 
 def main():
     
+    # Create an argument parser
     parser = argparse.ArgumentParser(description="File transferring application over 'DRTP'.")
     
+    # Add all options common for server and client
     parser.add_argument(
         '-i', '--ip', type=str, default='10.0.0.1', help="Enter server ip (default = 10.0.0.1)")
     parser.add_argument(
         '-p', '--port', type=int, default=24, action=PortInRangeAction, help="Enter server port (default = 24)")
     parser.add_argument(
-        '-t', '--test_case', type=int, default=1, action=TestInRangeAction, help="Enter testcase scenario")
-    parser.add_argument(
         '-r', '--reliable_method', type=str, default='stop_and_wait', action=ValidMethodAction, help="Enter one of three reliability functions (stop_and_wait, GBN, GBN-SR)")
     
+    # Create a group for server-arguments
     server_parser = parser.add_argument_group('Server')
-    
+    # Add all available options to invoke the server 
     server_parser.add_argument(
         '-s', '--server', action='store_true', help='Invoke as server (receiver)')
     
+    # Create a group for client-arguments
     client_parser = parser.add_argument_group('Client')
-    
+    # Add all available options to invoke the client
     client_parser.add_argument(
         '-c', '--client', action='store_true', help='Invoke as client (sender)')
     client_parser.add_argument(
@@ -555,14 +632,16 @@ def main():
     client_parser.add_argument(
         '-w', '--window', type=int, default='5', help="Enter window size of datapackets (default = 3 for GBN and GBN-SR)")
     
+    # Parse the commands line arguments
     args = parser.parse_args()
     
+    # If program is invoked as server
     if args.server:
         server_start(args)
-    
+    # If program is invoked as client
     elif args.client:
         client_connect(args)
-        
+    # If program is invoked as both  
     else:
         print("Error: you must run either in server or client mode")
         parser.print_help()
